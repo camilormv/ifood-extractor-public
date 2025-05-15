f9b9ea80dfd8a79a13132d87ffba3fbab2b25040
Camilo Maldonado <camilo.mv@gmail.com>
2025-05-14 23:26:35 -0400

    Olaclick Funcionando

--- test_playwright.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    print("Ruta del ejecutable Chromium:", p.chromium.executable_path)
-    browser = p.chromium.launch(headless=True)
-    print("¡Chromium abierto correctamente!")
-    browser.close()
-    
